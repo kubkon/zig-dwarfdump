84727ccce590046196b6016cb29b1c0936d60c69
Jakub Konka <kubkon@jakubkonka.com>
2022-09-30 10:36:24 +0200

    dwarfdump: add basic debug info extractor for ELF
    May crash as we don't handle all TAGs yet. Additionally, for
    relocatables, we also need to parse relocs to get different offsets
    such as an offset into the string table to get the names out, etc.

--- src/Context.zig
+++ src/Context.zig
@@ -1,6 +1,7 @@
 const Context = @This();
 
 const std = @import("std");
+const elf = std.elf;
 const macho = std.macho;
 const mem = std.mem;
 
@@ -9,22 +10,28 @@ debug_abbrev: []const u8,
 debug_str: []const u8,
 
 pub fn parse(data: []const u8) !Context {
-    var stream = std.io.fixedBufferStream(data);
-    const reader = stream.reader();
-
     // Try MachO first
-    const header = try reader.readStruct(macho.mach_header_64);
-    if (header.magic == macho.MH_MAGIC_64) {
-        return parseMachO(data);
+    {
+        const header = @ptrCast(*const macho.mach_header_64, data.ptr).*;
+        if (header.magic == macho.MH_MAGIC_64) {
+            return parseMachO(data);
+        }
+    }
+
+    // Next, let's try ELF
+    {
+        // TODO: 32bit ELF files
+        const header = @ptrCast(*const elf.Elf64_Ehdr, data.ptr).*;
+        if (mem.eql(u8, "\x7fELF", header.e_ident[0..4])) {
+            return parseElf(data);
+        }
     }
 
     return error.TODOOtherBackends;
 }
 
 fn parseMachO(data: []const u8) !Context {
-    var stream = std.io.fixedBufferStream(data);
-    const reader = stream.reader();
-    const header = try reader.readStruct(macho.mach_header_64);
+    const header = @ptrCast(*const macho.mach_header_64, data.ptr).*;
 
     var debug_info_h: ?macho.section_64 = null;
     var debug_abbrev_h: ?macho.section_64 = null;
@@ -68,5 +75,56 @@ fn parseMachO(data: []const u8) !Context {
         .debug_info = debug_info,
         .debug_abbrev = debug_abbrev,
         .debug_str = debug_string,
+    };
+}
+
+fn parseElf(data: []const u8) !Context {
+    const header = @ptrCast(*const elf.Elf64_Ehdr, data.ptr).*;
+
+    const shdrs = @ptrCast(
+        [*]const elf.Elf64_Shdr,
+        @alignCast(@alignOf(elf.Elf64_Shdr), data.ptr + header.e_shoff),
+    )[0..header.e_shnum];
+
+    const shstrtab = blk: {
+        const shdr = shdrs[header.e_shstrndx];
+        break :blk data[shdr.sh_offset..][0..shdr.sh_size];
+    };
+
+    var debug_info_h: ?elf.Elf64_Shdr = null;
+    var debug_abbrev_h: ?elf.Elf64_Shdr = null;
+    var debug_str_h: ?elf.Elf64_Shdr = null;
+
+    for (shdrs) |shdr| switch (shdr.sh_type) {
+        elf.SHT_PROGBITS => {
+            const sh_name = mem.sliceTo(@ptrCast([*:0]const u8, shstrtab.ptr + shdr.sh_name), 0);
+            if (mem.eql(u8, sh_name, ".debug_info")) {
+                debug_info_h = shdr;
+            }
+            if (mem.eql(u8, sh_name, ".debug_abbrev")) {
+                debug_abbrev_h = shdr;
+            }
+            if (mem.eql(u8, sh_name, ".debug_str")) {
+                debug_str_h = shdr;
+            }
+        },
+        else => {},
+    };
+
+    if (debug_info_h == null or debug_abbrev_h == null or debug_str_h == null) {
+        return error.MissingDebugInfo;
+    }
+
+    const dih = debug_info_h.?;
+    const dah = debug_abbrev_h.?;
+    const dsh = debug_str_h.?;
+    const debug_info = data[dih.sh_offset..][0..dih.sh_size];
+    const debug_abbrev = data[dah.sh_offset..][0..dah.sh_size];
+    const debug_str = data[dsh.sh_offset..][0..dsh.sh_size];
+
+    return Context{
+        .debug_info = debug_info,
+        .debug_abbrev = debug_abbrev,
+        .debug_str = debug_str,
     };
 }
