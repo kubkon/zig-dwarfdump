cb431c412fed7a1ab355fafab6674d7b21b01aac
Jakub Konka <kubkon@jakubkonka.com>
2023-09-01 23:59:33 +0200

    remove submodules and self-parse args

--- src/main.zig
+++ src/main.zig
@@ -1,49 +1,134 @@
 const std = @import("std");
-const clap = @import("clap");
 const process = std.process;
 
 const DwarfDump = @import("DwarfDump.zig");
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const usage =
+    \\Usage: zig-dwarfdump [options] file
+    \\
+    \\General options:
+    \\--debug-info          Display .debug_info section contents (default)
+    \\--eh-frame            Display .eh_frame section contents
+    \\--llvm-compatibility  Output is formatted exactly like llvm-dwarfdump, with no extra information
+    \\--all, -a             Display all debug info sections
+    \\--help                Display this help and exit
+    \\
+;
+
+fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    ret: {
+        const msg = std.fmt.allocPrint(gpa, format ++ "\n", args) catch break :ret;
+        std.io.getStdErr().writeAll(msg) catch {};
+    }
+    std.process.exit(1);
+}
+
+const ArgsIterator = struct {
+    args: []const []const u8,
+    i: usize = 0,
+
+    fn next(it: *@This()) ?[]const u8 {
+        if (it.i >= it.args.len) {
+            return null;
+        }
+        defer it.i += 1;
+        return it.args[it.i];
+    }
+
+    fn nextOrFatal(it: *@This()) []const u8 {
+        return it.next() orelse fatal("expected parameter after {s}", .{it.args[it.i - 1]});
+    }
+};
+
+var global_alloc = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = global_alloc.allocator();
 
 pub fn main() !void {
-    const stderr = std.io.getStdErr().writer();
-    const stdout = std.io.getStdOut().writer();
+    var arena_allocator = std.heap.ArenaAllocator.init(gpa);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
 
-    const params = comptime [_]clap.Param(clap.Help){
-        clap.parseParam("--help                 Display this help and exit.") catch unreachable,
-        clap.parseParam("--eh-frame             Display .eh_frame section contents.") catch unreachable,
-        clap.parseParam("--llvm-compatibility   Output is formatted exactly like llvm-dwarfdump, with no extra information.") catch unreachable,
-        clap.parseParam("<FILE>") catch unreachable,
+    const all_args = try std.process.argsAlloc(arena);
+    const args = all_args[1..];
+
+    if (args.len == 0) fatal(usage, .{});
+
+    var filename: ?[]const u8 = null;
+    var llvm_compat: bool = false;
+
+    const PrintMatrix = packed struct {
+        debug_info: bool = true,
+        eh_frame: bool = false,
+
+        const Int = blk: {
+            const bits = @typeInfo(@This()).Struct.fields.len;
+            break :blk @Type(.{
+                .Int = .{
+                    .signedness = .unsigned,
+                    .bits = bits,
+                },
+            });
+        };
+
+        fn enableAll() @This() {
+            return @as(@This(), @bitCast(~@as(Int, 0)));
+        }
+
+        fn isSet(pm: @This()) bool {
+            return @as(Int, @bitCast(pm)) != 0;
+        }
+
+        fn add(pm: *@This(), other: @This()) void {
+            pm.* = @as(@This(), @bitCast(@as(Int, @bitCast(pm.*)) | @as(Int, @bitCast(other))));
+        }
     };
+    var print_matrix: PrintMatrix = .{};
 
-    const parsers = comptime .{
-        .FILE = clap.parsers.string,
-    };
+    var it = ArgsIterator{ .args = args };
+    while (it.next()) |arg| {
+        if (std.mem.startsWith(u8, arg, "-")) blk: {
+            var i: usize = 1;
+            var tmp = PrintMatrix{};
+            while (i < arg.len) : (i += 1) switch (arg[i]) {
+                '-' => break :blk,
+                'a' => tmp = PrintMatrix.enableAll(),
+                else => break :blk,
+            };
+            print_matrix.add(tmp);
+            continue;
+        }
 
-    var res = try clap.parse(clap.Help, &params, parsers, .{
-        .allocator = gpa.allocator(),
-        .diagnostic = null,
-    });
-    defer res.deinit();
-
-    if (res.args.help != 0) {
-        return printUsageWithHelp(stderr, params[0..]);
+        if (std.mem.eql(u8, arg, "--help")) {
+            fatal(usage, .{});
+        } else if (std.mem.eql(u8, arg, "--all")) {
+            print_matrix = PrintMatrix.enableAll();
+        } else if (std.mem.eql(u8, arg, "--debug-info")) {
+            // Do nothing
+        } else if (std.mem.eql(u8, arg, "--eh-frame")) {
+            print_matrix.eh_frame = true;
+        } else if (std.mem.eql(u8, arg, "--llvm-compatibility")) {
+            llvm_compat = true;
+        } else {
+            if (filename != null) fatal("too many positional arguments specified", .{});
+            filename = arg;
+        }
     }
 
-    if (res.positionals.len == 0) {
-        return stderr.print("missing positional argument <FILE>...\n", .{});
-    }
-
-    const filename = res.positionals[0];
-    const file = try std.fs.cwd().openFile(filename, .{});
+    const fname = filename orelse fatal("no input file specified", .{});
+    const file = try std.fs.cwd().openFile(fname, .{});
     defer file.close();
 
-    var dd = try DwarfDump.parse(gpa.allocator(), file);
+    var dd = try DwarfDump.parse(gpa, file);
     defer dd.deinit();
 
-    if (res.args.@"eh-frame" != 0) {
-        try stdout.print("{s}:\tfile format {s}{s}\n", .{ filename, switch (dd.ctx.tag) {
+    const stdout = std.io.getStdOut().writer();
+
+    if (print_matrix.debug_info) {
+        try dd.printCompileUnits(stdout);
+        try stdout.writeAll("\n");
+    }
+    if (print_matrix.eh_frame) {
+        try stdout.print("{s}:\tfile format {s}{s}\n", .{ fname, switch (dd.ctx.tag) {
             .elf => "elf64-",
             .macho => "Mach-O ",
         }, if (dd.ctx.getArch()) |arch| switch (arch) {
@@ -52,13 +137,7 @@ pub fn main() !void {
             else => @tagName(arch),
         } else "unknown" });
 
-        try dd.printEhFrames(stdout, res.args.@"llvm-compatibility" != 0);
-    } else try dd.printCompileUnits(stdout);
-}
-
-fn printUsageWithHelp(stream: anytype, comptime params: []const clap.Param(clap.Help)) !void {
-    try stream.print("dwarfdump ", .{});
-    try clap.usage(stream, clap.Help, params);
-    try stream.print("\n", .{});
-    try clap.help(stream, clap.Help, params, .{});
+        try dd.printEhFrames(stdout, llvm_compat);
+        try stdout.writeAll("\n");
+    }
 }
